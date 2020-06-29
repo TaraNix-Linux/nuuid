4c82d38db522704d9540caf16922918ed4cf0e6f
Diana <5275194+DianaNites@users.noreply.github.com>
2020-06-29 01:27:01 -0400

    Namespaces

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,26 @@ const UUID_STR_LENGTH: usize = 36;
 const UUID_URN_LENGTH: usize = 45;
 const UUID_URN: &str = "urn:uuid:";
 
+/// The predefined DNS namespace, 6ba7b810-9dad-11d1-80b4-00c04fd430c8.
+pub const NAMESPACE_DNS: Uuid = Uuid::from_bytes([
+    107, 167, 184, 16, 157, 173, 17, 209, 128, 180, 0, 192, 79, 212, 48, 200,
+]);
+
+/// The predefined URL namespace, 6ba7b811-9dad-11d1-80b4-00c04fd430c8.
+pub const NAMESPACE_URL: Uuid = Uuid::from_bytes([
+    107, 167, 184, 17, 157, 173, 17, 209, 128, 180, 0, 192, 79, 212, 48, 200,
+]);
+
+/// The predefined OID namespace, 6ba7b812-9dad-11d1-80b4-00c04fd430c8.
+pub const NAMESPACE_OID: Uuid = Uuid::from_bytes([
+    107, 167, 184, 18, 157, 173, 17, 209, 128, 180, 0, 192, 79, 212, 48, 200,
+]);
+
+/// The predefined X500 namespace, 6ba7b814-9dad-11d1-80b4-00c04fd430c8.
+pub const NAMESPACE_X500: Uuid = Uuid::from_bytes([
+    107, 167, 184, 20, 157, 173, 17, 209, 128, 180, 0, 192, 79, 212, 48, 200,
+]);
+
 /// A 16 byte with the UUID.
 pub type Bytes = [u8; 16];
 
